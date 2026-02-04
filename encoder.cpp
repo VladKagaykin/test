f47e5c61f2ef43d3dfab43d3ad2880840a7c7127
AVCD <vladkagajkin233@gmail.com>
2026-02-04 20:54:48 +0300

    end of testing

--- encoder.cpp
+++ encoder.cpp
@@ -1,9 +1,9 @@
-#include <opencv2/opencv.hpp>
+#include<opencv2/opencv.hpp>
 #include<iostream>
 #include<string>
-#include <clocale>
-#include <cwchar>
-#include <iomanip>
+#include<clocale>
+#include<cwchar>
+#include<iomanip>
 
 using namespace cv;
 using namespace std;
@@ -14,15 +14,12 @@ int window_image(string load_image) {
         cout << "Unable to read image: " << load_image << endl;
         return -1;
     }
-
     int original_width = image.cols;
     int original_height = image.rows;
-
     for (int i = 0; i < original_height; i++) {
         for (int j = 0; j < original_width; j++) {
             Vec3b pixel = image.at<Vec3b>(i, j);
-
-            cout << hex 
+            cout<< hex
                 << (int)pixel[2]   // R
                 << (int)pixel[1]   // G
                 << (int)pixel[0]   // B
@@ -58,57 +55,56 @@ void encode_text_to_image(string image_path, string text) {
         cout << "Unable to read image: " << image_path << endl;
         return;
     }
-
-    string hex_text = text_to_hex(text);
     int original_width = image.cols;
     int original_height = image.rows;
-    int total_pixels = image.rows * image.cols;
+    int total_pixels = original_width * original_height;
     int max_text_length = total_pixels * 3;
-    
-    if (hex_text.length() > max_text_length) {
+    if ((int)text.length() > max_text_length) {
         cout << "Text too long for this image!" << endl;
         return;
     }
-
     int idx = 0;
-    for (int i = 0; i < original_height && idx < hex_text.length(); i++) {
-        for (int j = 0; j < original_width && idx < hex_text.length(); j++) {
+    for (int i = 0; i < original_height && idx < (int)text.length(); i++) {
+        for (int j = 0; j < original_width && idx < (int)text.length(); j++) {
             Vec3b& pixel = image.at<Vec3b>(i, j);
-            if (idx < hex_text.length()) pixel[2] = hex_text[idx++];
-            if (idx < hex_text.length()) pixel[1] = hex_text[idx++];
-            if (idx < hex_text.length()) pixel[0] = hex_text[idx++];
+            
+            if (idx < (int)text.length()) pixel[2] = text[idx++];
+            if (idx < (int)text.length()) pixel[1] = text[idx++];
+            if (idx < (int)text.length()) pixel[0] = text[idx++];
         }
     }
     imwrite(image_path, image);
-    cout << image_path << " " << hex_text << endl;
 }
 
 string read_image(string load_image) {
     Mat image = imread(load_image);
     if (image.empty()) return "";
-
     string text = "";
-
     for (int i = 0; i < image.rows; i++) {
         for (int j = 0; j < image.cols; j++) {
             Vec3b pixel = image.at<Vec3b>(i, j);
-            text += (char)pixel[2]; // R
-            text += (char)pixel[1]; // G
-            text += (char)pixel[0]; // B
+            
+            text += (char)pixel[2]; 
+            text += (char)pixel[1]; 
+            text += (char)pixel[0]; 
         }
     }
     return text;
 }
 
+void create_void_image(string encode_text, string img_name, int height, int width){
+    Mat img(height, width, CV_8UC4, Scalar(0, 0, 0, 0));
+    imwrite(img_name, img);
+    encode_text_to_image(img_name,encode_text);
+}
 
 int main(){
-    setlocale(LC_ALL, "");
-    // window_image("penza_low.png");
-    // waitKey(0);
-    // cout<<text_to_hex("test▓")<<endl;
-    encode_text_to_image("penza_low_test.png","don`t talk to strangers");
-    // window_image("penza_low_test.png");
-    // waitKey(0);
-    cout<<read_image("penza_low_test.png");
+    // string text;
+    // getline(cin,text);
+    // setlocale(LC_ALL, "");
+    // create_void_image(text,"programm.png",64,64);
+    window_image("penza_low.png");
+    cout<<read_image("penza_low.png")<<endl;
+    waitKey(0);
     return 0;
 }
